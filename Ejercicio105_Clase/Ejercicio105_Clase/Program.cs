801d420e1a726fd1b9a0879c3c44952c5a2686d4
Ruben Calero <rcalerocordoba@gmail.com>
2025-10-27 20:56:30 +0100

    V.Final - Solucion de Ana

--- Ejercicio105_Clase/Ejercicio105_Clase/Program.cs
+++ Ejercicio105_Clase/Ejercicio105_Clase/Program.cs
@@ -4,17 +4,59 @@
     {
         static void Main(string[] args)
         {
-            double[] numeros = new double[] { 185.50, 250.36, 163.45, 535.20, 950.22, 450.38 };
-
-
-            for (int i = 0; i < numeros.Length; i++)
+            double[] semana = new double[6];
+            string[] DIAS = { "MARTES", "MIERCOLES", "JUEVES", "VIERNES", "SABADO", "DOMINGO" };
+            bool maxEmpate = false, minEmpate = false;
+            double maxValue = int.MinValue, minValue = int.MaxValue, average = 0;
+            int maxDay = 0, minDay = 0;
+            for (int i = 0; i < 6; i++)
             {
-                double numTempMax = numeros[i];
-                double numTempMin = numeros[i];
-                if (numeros[i] >
+                double ventas = double.Parse(Console.ReadLine());
+                semana[i] = ventas;
+                average += ventas;
 
+                if (ventas > maxValue)
+                {
+                    maxValue = ventas;
+                    maxEmpate = false;
+                    maxDay = i;
+                }
+                else if (ventas == maxValue)
+                {
+                    maxEmpate = true;
+                }
 
+                if (ventas < minValue)
+                {
+                    minValue = ventas;
+                    minEmpate = false;
+                    minDay = i;
+                }
+                else if (ventas == minValue)
+                {
+                    minEmpate = true;
+                }
             }
+
+
+            if (maxEmpate)
+            {
+                Console.Write("EMPATE");
+            }
+            else Console.Write(DIAS[maxDay]);
+
+            Console.Write(' ');
+
+            if (minEmpate)
+            {
+                Console.Write("EMPATE");
+            }
+            else Console.Write(DIAS[minDay]);
+
+            Console.Write(' ');
+
+            if (average / 6 > semana[5]) Console.WriteLine("NO");
+            else Console.WriteLine("SI");
         }
     }
 }
