3d209ac631d57ae621c3635975f1ec2ebe70b241
Ruben Calero <rcalerocordoba@gmail.com>
2025-10-27 20:51:25 +0100

    V1 - Primer intento del ejercicio. Solo me dio tiempo a intentar plantearlo

---
+++ Ejercicio105_Clase/Ejercicio105_Clase/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Ejercicio105_Clase
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] numeros = new double[] { 185.50, 250.36, 163.45, 535.20, 950.22, 450.38 };
+
+
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                double numTempMax = numeros[i];
+                double numTempMin = numeros[i];
+                if (numeros[i] >
+
+
+            }
+        }
+    }
+}
